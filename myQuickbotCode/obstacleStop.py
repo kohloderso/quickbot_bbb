261b1e398ba27881ff4ca3b83cb666f4a88ab6d3
christina <christina.kohl@student.uibk.ac.at>
2015-02-03 17:51:03 +0100

    first obstacle test

--- myQuickbotCode/obstacleStop.py
+++ myQuickbotCode/obstacleStop.py
@@ -1,1 +1,48 @@
+import quickbot_config as config
+import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.PWM as PWM
+import Adafruit_BBIO.GPIO as GPIO
+import time
 
+LEFT = 0
+RIGHT = 1
+MIN = 0
+MAX = 1
+
+ADC.setup()
+
+# Motor Pins -- (LEFT, RIGHT)
+dir1Pin = (config.INl1, config.INr1)
+dir2Pin = (config.INl2, config.INr2)
+pwmPin = (config.PWMl, config.PWMr)
+
+"""Initialize GPIO pins"""
+GPIO.setup(dir1Pin[LEFT], GPIO.OUT)
+GPIO.setup(dir2Pin[LEFT], GPIO.OUT)
+GPIO.setup(dir1Pin[RIGHT], GPIO.OUT)
+GPIO.setup(dir2Pin[RIGHT], GPIO.OUT)
+
+PWM.start(pwmPin[LEFT], 0)#, frequency=frequency)
+PWM.start(pwmPin[RIGHT], 0)#, frequency=frequency)
+
+obstacle = False
+while not obstacle:
+    GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
+    GPIO.output(dir2Pin[LEFT], GPIO.LOW)
+    PWM.set_duty_cycle(pwmPin[LEFT], 50)
+    GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
+    GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
+    PWM.set_duty_cycle(pwmPin[RIGHT], 50)
+
+    if ADC.read(config.IRfl > 0.3):
+        obstacle = True
+    elif ADC.read(config.IRfr > 0.3):
+        obstacle = True
+    elif ADC.read(config.IRfm > 0.3):
+        obstacle = True
+
+PWM.stop(pwmPin[LEFT])
+PWM.stop(pwmPin[RIGHT])
+PWM.cleanup()
+
+GPIO.cleanup()
