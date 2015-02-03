91a4923ba154998e0f0876748b02ace861d28e51
christina <christina.kohl@student.uibk.ac.at>
2015-02-03 19:05:55 +0100

    obstacle avoidance first try

--- myQuickbotCode/obstacleStop.py
+++ myQuickbotCode/obstacleStop.py
@@ -25,41 +25,50 @@ GPIO.setup(dir2Pin[RIGHT], GPIO.OUT)
 PWM.start(pwmPin[LEFT], 0)#, frequency=frequency)
 PWM.start(pwmPin[RIGHT], 0)#, frequency=frequency)
 
-obstacle = False
-while not obstacle:
-    GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
-    GPIO.output(dir2Pin[LEFT], GPIO.LOW)
-    PWM.set_duty_cycle(pwmPin[LEFT], 50)
-    GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
-    GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
-    PWM.set_duty_cycle(pwmPin[RIGHT], 50)
-
-    reading1 = ADC.read(config.IRfl)
-    reading2 = ADC.read(config.IRfr)
-    reading3 = ADC.read(config.IRfm)
-    print("sensor left " + str(reading1))
-    print("sensor right " + str(reading2))
-    print("sensor middle " + str(reading3))
-
-    if max(reading1, reading2, reading3) > 0.5:
-        obstacle = True
-
-# while True:
-#     leftIR = ADC.read(config.IRfl)
-#     rightIR = ADC.read(config.IRfr)
-#     middleIR = ADC.read(config.IRfm)
+# obstacle = False
+# while not obstacle:
+#     GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
+#     GPIO.output(dir2Pin[LEFT], GPIO.LOW)
+#     PWM.set_duty_cycle(pwmPin[LEFT], 50)
+#     GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
+#     GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
+#     PWM.set_duty_cycle(pwmPin[RIGHT], 50)
+#
+#     reading1 = ADC.read(config.IRfl)
+#     reading2 = ADC.read(config.IRfr)
+#     reading3 = ADC.read(config.IRfm)
 #     print("sensor left " + str(reading1))
 #     print("sensor right " + str(reading2))
 #     print("sensor middle " + str(reading3))
 #
-#     if leftIR > 0.3:
-#         GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
-#         GPIO.output(dir2Pin[LEFT], GPIO.LOW)
-#         PWM.set_duty_cycle(pwmPin[LEFT], 0)
-#     elif rightIR > 0.3:
-#         GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
-#         GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
-#         PWM.set_duty_cycle(pwmPin[RIGHT], 0)
+#     if max(reading1, reading2, reading3) > 0.5:
+#         obstacle = True
+
+middleIR = 0
+
+while middleIR < 0.6:
+    leftIR = ADC.read(config.IRfl)
+    rightIR = ADC.read(config.IRfr)
+    middleIR = ADC.read(config.IRfm)
+    print("sensor left " + str(leftIR))
+    print("sensor right " + str(rightIR))
+    print("sensor middle " + str(middleIR))
+
+    if rightIR > 0.5:
+        GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
+        GPIO.output(dir2Pin[LEFT], GPIO.LOW)
+        PWM.set_duty_cycle(pwmPin[LEFT], 50)
+    elif leftIR > 0.5:
+        GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
+        GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
+        PWM.set_duty_cycle(pwmPin[RIGHT], 50)
+    else:
+        GPIO.output(dir1Pin[LEFT], GPIO.HIGH)
+        GPIO.output(dir2Pin[LEFT], GPIO.LOW)
+        PWM.set_duty_cycle(pwmPin[LEFT], 50)
+        GPIO.output(dir1Pin[RIGHT], GPIO.HIGH)
+        GPIO.output(dir2Pin[RIGHT], GPIO.LOW)
+        PWM.set_duty_cycle(pwmPin[RIGHT], 50)
 
 
 PWM.stop(pwmPin[LEFT])
